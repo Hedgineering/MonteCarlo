54db898c5bd75dd7d2fc08f3491af9b8f31258b6
vriishin <95161161+vriishin@users.noreply.github.com>
2023-03-28 10:58:43 -0400

    Added getMarketData function for Marketstack data

---
+++ server/helpers/getMarketData.js
@@ -0,0 +1,22 @@
+/* 
+ * @params ticker is the shorthand letter code for a stock (e.g. AAPL for Apple Inc.) 
+ * @params dataType is the kind of data you would like to request from Marketstack's API: eod for end-of-day info; intraday for intraday info; dividends for dividends info
+ */
+function getMarketData(ticker,dataType) {
+    var host = 'http://api.marketstack.com/v1/';
+    var apiKey = 'cd8eb947528ad20baa21b89e44a161c7';    
+    const link = JSON.stringify(host + dataType +  '?' + 'access_key=' + apiKey + '&' + 'symbols=' + ticker );
+
+    let url = new URL(host + dataType +  '?' + 'access_key=' + apiKey + '&' + 'symbols=' + ticker );
+
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(users => {
+            console.log(users);
+        });
+
+
+    }
