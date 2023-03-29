8fd794de68e066540612f6188c6832c0ca7338e1
Rahul Hegde <rhegde.hrahul@gmail.com>
2023-03-29 13:00:52 -0400

    added small some boilerplate equity charts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import sampleData from './sample-data/sampleEquity.json'
 import EquityChart from './components/EquityChart'
 import { useEffect } from 'react'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   const smallData  : number[][] = JSON.parse(JSON.stringify(sampleData))["small"];
-//   const mediumData : number[][] = JSON.parse(JSON.stringify(sampleData))["medium"];
-//   const largeData  : number[][] = JSON.parse(JSON.stringify(sampleData))["large"];
-//   const xlargeData : number[][] = JSON.parse(JSON.stringify(sampleData))["xlarge"];
-
-//   console.log("sampleData: " + JSON.stringify(sampleData));
-//   console.log("smallData: " + JSON.stringify(smallData));
-//   console.log("mediumData: " + JSON.stringify(mediumData));
-//   console.log("largeData: " + JSON.stringify(largeData));
-//   console.log("xlargeData: " + JSON.stringify(xlargeData));
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src="/vite.svg" className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <EquityChart data={smallData} />
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>
-//   )
-// }
+import { Container } from 'react-bootstrap'
 
 const App: React.FC = () => {
   const [isReady, setIsReady] = useState(false);
@@ -61,10 +22,19 @@ const App: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1>Equity Chart</h1>
+    <Container>
+      <h2 className='text-center'>Small Equity Chart</h2>
       <EquityChart data={smallData} />
-    </div>
+
+      <h2 className='text-center'>Medium Equity Chart</h2>
+      <EquityChart data={mediumData} />
+
+      <h2 className='text-center'>Large Equity Chart</h2>
+      <EquityChart data={largeData} />
+
+      <h2 className='text-center'>X-Large Equity Chart</h2>
+      <EquityChart data={xlargeData} />
+    </Container>
   );
 };
 
