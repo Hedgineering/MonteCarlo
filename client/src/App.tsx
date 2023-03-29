383af9365bea801b5253be52680fed651ce8d3b7
Rahul Hegde <rhegde.hrahul@gmail.com>
2023-03-29 12:49:32 -0400

    added react boostrap and react-chartjs-2 and simple equity chart functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,71 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import sampleData from './sample-data/sampleEquity.json'
+import EquityChart from './components/EquityChart'
+import { useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   const smallData  : number[][] = JSON.parse(JSON.stringify(sampleData))["small"];
+//   const mediumData : number[][] = JSON.parse(JSON.stringify(sampleData))["medium"];
+//   const largeData  : number[][] = JSON.parse(JSON.stringify(sampleData))["large"];
+//   const xlargeData : number[][] = JSON.parse(JSON.stringify(sampleData))["xlarge"];
+
+//   console.log("sampleData: " + JSON.stringify(sampleData));
+//   console.log("smallData: " + JSON.stringify(smallData));
+//   console.log("mediumData: " + JSON.stringify(mediumData));
+//   console.log("largeData: " + JSON.stringify(largeData));
+//   console.log("xlargeData: " + JSON.stringify(xlargeData));
+
+//   return (
+//     <div className="App">
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src="/vite.svg" className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <EquityChart data={smallData} />
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </div>
+//   )
+// }
+
+const App: React.FC = () => {
+  const [isReady, setIsReady] = useState(false);
+  const smallData  : number[][] = JSON.parse(JSON.stringify(sampleData))["small"];
+  const mediumData : number[][] = JSON.parse(JSON.stringify(sampleData))["medium"];
+  const largeData  : number[][] = JSON.parse(JSON.stringify(sampleData))["large"];
+  const xlargeData : number[][] = JSON.parse(JSON.stringify(sampleData))["xlarge"];
+
+  useEffect(() => {
+    setIsReady(true);
+  }, []);
+
+  if (!isReady) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Equity Chart</h1>
+      <EquityChart data={smallData} />
     </div>
-  )
-}
+  );
+};
 
 export default App
--- client/src/components/EquityChart.tsx
+++ client/src/components/EquityChart.tsx
@@ -1,52 +1,59 @@
-import React, { useEffect, useRef } from 'react';
-import Chart from 'chart.js';
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
 
-const EquityChart = ({ data }) => {
-  const canvasRef = useRef(null);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+import { Line } from 'react-chartjs-2';
 
-  useEffect(() => {
-    if (!canvasRef.current) return;
+type EquityChartProps = {
+  data: number[][];
+};
 
-    const chartData = {
-      labels: Array.from({ length: data[0].length }, (_, i) => `Trade ${i + 1}`),
-      datasets: data.map((values, i) => ({
-        label: `Equity ${i + 1}`,
-        data: values,
-        borderColor: `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 1)`,
-        fill: false,
-      })),
-    };
+const EquityChart: React.FC<EquityChartProps> = ({ data }) => {
+  const chartData = {
+    labels: Array.from({ length: data[0].length }, (_, i) => `${i}`),
+    datasets: data.map((values, i) => ({
+      label: `Equity ${i + 1}`,
+      data: values,
+      borderColor: `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 1)`,
+      fill: false,
+    })),
+  };
 
-    const chartOptions = {
-      responsive: true,
-      scales: {
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Number of Trades',
-          },
-        }],
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Equity',
-          },
-        }],
+  const chartOptions = {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Number of Trades',
+        },
       },
-    };
+      y: {
+        title: {
+          display: true,
+          text: 'Equity',
+        },
+      },
+    },
+  };
 
-    const chart = new Chart(canvasRef.current, {
-      type: 'line',
-      data: chartData,
-      options: chartOptions,
-    });
-
-    return () => {
-      chart.destroy();
-    };
-  }, [data]);
-
-  return <canvas ref={canvasRef} />;
+  return <Line data={chartData} options={chartOptions} />;
 };
 
 export default EquityChart;
