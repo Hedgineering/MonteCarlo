b475b85c71a1763765204787a599703b2bfcfce9
Rahul Hegde <rhegde.hrahul@gmail.com>
2023-04-18 01:16:21 -0400

    i'm dying again

--- client/src/components/SimulationForm.tsx
+++ client/src/components/SimulationForm.tsx
@@ -21,7 +21,7 @@ const SimulationForm: React.FC<SimulationFormProps> = ({ setData, setStatistics,
     event.preventDefault();
 
     console.log("sending request...");
-    const destination = `/api/simulate`;
+    const destination = `${serverIp}/api/simulate`;
     console.log("sending request to " + destination)
 
     const response = await fetch(destination, {
