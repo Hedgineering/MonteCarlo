8f3cc9a2cb175254d47cdafedf548e9a62ab7399
Rahul Hegde <rhegde.hrahul@gmail.com>
2023-04-17 23:27:21 -0400

    fixed fetch error

--- client/src/components/SimulationForm.tsx
+++ client/src/components/SimulationForm.tsx
@@ -22,7 +22,7 @@ const SimulationForm: React.FC<SimulationFormProps> = ({ setData, setStatistics,
 
     console.log("sending request...");
 
-    const response = await fetch("http://localhost:5002/api/simulate", {
+    const response = await fetch(`${serverIp}/api/simulate`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
